ce658780f1e416af1de82a210c05dbc0263c5f37
pado0 <bgshhd95@gmail.com>
2022-05-12 10:52:00 +0900

    ex1_calculator in kt

--- src/ex1_calculator/Main_kt.kt
+++ src/ex1_calculator/Main_kt.kt
@@ -14,29 +14,29 @@ fun printBill(capa:Int) {
 }
 
 fun ElectricBillCalculate(kwh: Int) : Double {
-    var result = 0.0; // 가변변수 선언 IN DOUBLE
-    var tkwh = kwh; // 코틀린은 매개변수 값 변경 불가하므로 별도 변수로 선언
+    var result = 0.0 // 가변변수 선언 IN DOUBLE
+    var tkwh = kwh // 코틀린은 매개변수 값 변경 불가하므로 별도 변수로 선언
     while (tkwh > 0) {
         if (tkwh <= 100) {
-            result += tkwh * 60.7;
-            tkwh = 0;
+            result += tkwh * 60.7
+            tkwh = 0
         } else if (tkwh in 101..200) { // 범위지정 연산자로 변환. a..b : a<=x<=b
-            result += (kwh - 100) * 125.9;
-            tkwh = 100;
+            result += (kwh - 100) * 125.9
+            tkwh = 100
         } else if (tkwh in 201..300) {
-            result += (kwh - 200) * 187.9;
-            tkwh = 200;
+            result += (kwh - 200) * 187.9
+            tkwh = 200
         } else if (tkwh in 301..400) {
-            result += (kwh - 300) * 280.6;
-            tkwh = 300;
+            result += (kwh - 300) * 280.6
+            tkwh = 300
         } else if (tkwh in 401..500) {
-            result += (tkwh - 400) * 417.7;
-            tkwh = 400;
+            result += (tkwh - 400) * 417.7
+            tkwh = 400
         } else {
-            result += (tkwh - 500) * 670.6;
-            tkwh = 500;
+            result += (tkwh - 500) * 670.6
+            tkwh = 500
         }
     }
-    return result;
+    return result
 }
 
