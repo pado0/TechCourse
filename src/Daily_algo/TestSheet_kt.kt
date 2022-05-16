602f09a36db3afd8739b9a3dfae133a206db33a5
pado0 <bgshhd95@gmail.com>
2022-05-16 09:47:53 +0900

    daily algo

--- src/Daily_algo/TestSheet_kt.kt
+++ src/Daily_algo/TestSheet_kt.kt
@@ -1,23 +1,24 @@
 package Daily_algo
-fun main(args: Array<String>) {
 
-    val person = Person("hyojin", 28)
-    person.showMe();
-    person.showAge(person)
+fun main(args: Array<String>) {
+    val person = Person()
+    person.age = 11;
+    person.showMe()
 }
 
-internal class Person{
-    var name: String = "";
-    var age: Int = 0;
+class Person{
+    var name: String = ""
+        get(): String = "getter test"
 
-    constructor(name: String, age: Int) {
-        this.name = name
-        this.age = age
-    }
+    var age : Int = 0;
 
-
-    internal fun showAge(p: Person) = println(p.age)
     fun showMe(){
         println("$name, $age")
-    }
+    // showMe에서 name 읽을때 자동으로 get()호출. setter같은데;...?
+        // 왜 hyo가 안들어가는지 이해가 안간다.
+    // showMe에서 name 호출 -> person.name 이 됨
+        // == person.getName == 커스텀 게터의 리턴값 getter test가 반환됨
+    //person.getName()
+
+   }
 }
--- src/Daily_algo/ex25_power_logn.java
+++ src/Daily_algo/ex25_power_logn.java
@@ -1,18 +1,21 @@
 package Daily_algo;
 
-import java.util.*;
+import java.util.HashMap;
+/*
+// 사용한 생각
+O(LogN) 이려면 분할정복 / 투포인터.. 같은 반씩 쪼개는 알고리즘 사용해야함.
+2^5 = 2^2 * 2^2 * 2
+    = (2^1*2^1) * (2^1*2^1) * 2
 
+3^5 = 3^2 * 3^2 * 3^1
+내가 이전에 구현한건 O(N)
+ */
 
-
-public class TestSheet {
-
+public class ex25_power_logn {
     static HashMap<Integer, Long> hashMap = new HashMap<>();
     final static Long DEVIDER = 94906249L;
 
     public static void main(String[] args) {
-
-        System.out.println(power(5, 22));
-
 
     }
 
@@ -20,6 +23,7 @@ public class TestSheet {
 
         if (exponent == 0) return 1;
         //if (exponent == 1) return base; //%2 조건 if 문에 걸려서 없어도 됨
+
 
         int devided = exponent / 2;
         Long tmp = power(base, devided);
@@ -29,6 +33,3 @@ public class TestSheet {
         return result_loop;
     }
 }
-
-
-
