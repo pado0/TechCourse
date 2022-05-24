cbf451d6c3c737bc6cc757af6a68497624a80f10
pado0 <bgshhd95@gmail.com>
2022-05-24 10:18:25 +0900

    pq

--- src/Daily_algo_baekjoon/growth2_1655.java
+++ src/Daily_algo_baekjoon/growth2_1655.java
@@ -1,41 +1,46 @@
 package Daily_algo_baekjoon;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /*
 * 백준이가 정수를 외칠때마다, 동생은 지금까지 말한 값 중 중간값을 말해야함
 * 짝수개라면 중간값 중 작은걸 말해야한다.
 *
-* 넣고 정렬
 * -> 정렬 말고 우선순위 큐로 풀기.
+* 그 수들의 가운데 있는 수를 말하는 것.
+*
 * */
 public class growth2_1655 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int N = scanner.nextInt();
-        int[] arr = new int[N];
+    public static void main(String[] args) throws IOException {
+        // 시간초과 원인 2
+        //Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = scanner.nextInt();
-            int[] tmp = new int[i+1];
-            for(int j=0 ; j<=i ; j++){
-                tmp[j] = arr[j];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 우선순위 큐 정렬방법
+        PriorityQueue<Integer> decPQ = new PriorityQueue<>((a,b) -> b - a);
+        PriorityQueue<Integer> incPQ = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (decPQ.size() == incPQ.size()) {
+                decPQ.add(x);
+            } else incPQ.add(x);
+
+            if (!decPQ.isEmpty() && !incPQ.isEmpty() && decPQ.peek() > incPQ.peek()) {
+                incPQ.add(decPQ.poll()); // 얘가 들어와도, inc의 peek는 변하지 않음
+                decPQ.add(incPQ.poll());
             }
-
-            Arrays.sort(tmp);
-            int l = tmp.length;
-            if(l%2 == 1) System.out.println(tmp[l/2]);
-            else System.out.println(tmp[l/2 -1]);
+            bw.write(decPQ.peek()+"\n");
+            //System.out.println(decPQ.peek());
         }
-        // 1 -> 0
-        // 1 5 -> 0
-        // 1 2 5 -> 1
-        // 1 2 5 10 -> 1
-        // 1 2 5 10 11 ->2
 
-        System.out.println();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
